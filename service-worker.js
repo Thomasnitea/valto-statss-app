ea761beae0b8647616accf8da82480ad8efa0818
t.ris <t.ris@nitea.nl>
2026-09-08 15:27:30 +0200

    Maak scoreboard-tabel compacter met scheidingslijnen tussen H1/H2/Tot, meer ruimte tussen percentages en score

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever any cached file changes, so clients pick up the update.
-const CACHE_NAME = 'korfbal-app-v9';
+const CACHE_NAME = 'korfbal-app-v10';
 
 const ASSETS = [
   './',
